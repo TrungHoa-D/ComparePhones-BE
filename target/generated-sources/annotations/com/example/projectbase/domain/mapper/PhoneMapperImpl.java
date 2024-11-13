cfd25e9bf1edf1ec06e79263fa2147c8f6f5177b
TrungHoa-D <trunghoa2k4@gmail.com>
2024-11-13 19:55:25 +0700

    fix: chang cors configuration

---
+++ target/generated-sources/annotations/com/example/projectbase/domain/mapper/PhoneMapperImpl.java
@@ -0,0 +1,42 @@
+package com.example.projectbase.domain.mapper;
+
+import com.example.projectbase.domain.dto.response.PhoneResponseDto;
+import com.example.projectbase.domain.entity.Phone;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-11-10T22:56:36+0700",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.12 (Ubuntu)"
+)
+@Component
+public class PhoneMapperImpl implements PhoneMapper {
+
+    @Override
+    public PhoneResponseDto toPhoneResponseDto(Phone phone) {
+        if ( phone == null ) {
+            return null;
+        }
+
+        PhoneResponseDto phoneResponseDto = new PhoneResponseDto();
+
+        phoneResponseDto.setId( phone.getId() );
+        phoneResponseDto.setName( phone.getName() );
+        phoneResponseDto.setBrand( phone.getBrand() );
+        phoneResponseDto.setReleaseDate( phone.getReleaseDate() );
+        phoneResponseDto.setCost( phone.getCost() );
+        phoneResponseDto.setImg( phone.getImg() );
+        phoneResponseDto.setColor( phone.getColor() );
+        phoneResponseDto.setScreen( phone.getScreen() );
+        phoneResponseDto.setCamera( phone.getCamera() );
+        phoneResponseDto.setProcessor( phone.getProcessor() );
+        phoneResponseDto.setConnection( phone.getConnection() );
+        phoneResponseDto.setStorage( phone.getStorage() );
+        phoneResponseDto.setBattery( phone.getBattery() );
+        phoneResponseDto.setDesign( phone.getDesign() );
+        phoneResponseDto.setOtherInfor( phone.getOtherInfor() );
+
+        return phoneResponseDto;
+    }
+}
